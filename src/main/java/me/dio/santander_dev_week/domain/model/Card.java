7cf52c34f52ab530f7f057d1f64454edf5f25dbc
Guilho-19 <guilhermepalaronunessilva@gmail.com>
2025-09-19 12:08:40 -0300

    Criação das Classes Account/Card/Feature/News/User

---
+++ src/main/java/me/dio/santander_dev_week/domain/model/Card.java
@@ -0,0 +1,18 @@
+package me.dio.santander_dev_week.domain.model;
+
+import java.math.BigDecimal;
+
+public class Card {
+	private Long id;
+	private String number;
+	private BigDecimal limit;
+	
+	public long getId() { return id; }
+	public void setId(Long id) { this.id = id; }
+	
+	public String getNumber() { return number; }
+	public void setNumber(String number) { this.number = number; }
+	
+	public BigDecimal getLimit() { return limit; }
+	public void setLimit(BigDecimal limit) { this.limit = limit; }
+}
---
+++ src/main/java/me/dio/santander_dev_week/domain/model/News.java
@@ -0,0 +1,16 @@
+package me.dio.santander_dev_week.domain.model;
+
+public class News {
+	private Long id;
+	private String icon;
+	private String description;
+	
+	public Long getId() { return id; }
+	public void setId() { this.id = id; }
+	
+	public String getIcon() { return icon; }
+	public void setIcon() { this.icon = icon; }
+	
+	public String getDescription() { return description; }
+	public void setDescription() { this.description = description; }
+}
