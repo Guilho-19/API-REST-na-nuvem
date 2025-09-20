d13af6048e8544397e01c16a1ab83b89b3e59b96
Guilho-19 <guilhermepalaronunessilva@gmail.com>
2025-09-19 22:35:10 -0300

    Criação de BD das classes User/Account

--- src/main/java/me/dio/santander_dev_week/domain/model/Account.java
+++ src/main/java/me/dio/santander_dev_week/domain/model/Account.java
@@ -2,11 +2,28 @@ package me.dio.santander_dev_week.domain.model;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_account")
 public class Account {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(unique = true)
 	private String number;
+	
 	private String agency;
+	
+	@Column(precision = 2, scale = 13)
 	private BigDecimal balance;
+	
+	@Column(precision = 2, scale = 13)
 	private BigDecimal limit;
 	
 	public Long getId() { return id; }
